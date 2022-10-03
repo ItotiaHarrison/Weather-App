bca762841b49643e3ffdac435c05b7083e1eb3f3
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-10-03 23:37:18 +0300

    useState useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
-}
+  const [latitude, setLatitude] = useState([]);
+  const [longitude, setLongitude] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    }, [latitude, longitude]);
+
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
 
 export default App;
