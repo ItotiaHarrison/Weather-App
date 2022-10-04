b96d0c1f4c1fb9072043b1be257ad11ed6addc0a
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-10-04 21:00:54 +0300

    add icons to forecast

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -16,7 +16,46 @@ import {
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const Forecast = () => {
+function Forecast(props, { weatherData }) {
+    const WeatherIcon = styled.div`
+  color: whitesmoke;`;
+
+    const { forecast } = props;
+
+    const results = forecast.map((item, index) => {
+
+        let weatherIcon = null;
+    
+        if (item.description === 'Thunderstorm') {
+          weatherIcon = <FontAwesomeIcon icon={faBolt} />;
+        }else if (item.description === 'Drizzle') {
+          weatherIcon = <FontAwesomeIcon icon={faCloudRain} />;
+        } else if (item.description === 'Rain') {
+          weatherIcon = <FontAwesomeIcon icon={faCloudShowersHeavy} />;
+        } else if (item.description === 'Snow') {
+          weatherIcon = <FontAwesomeIcon icon={faSnowflake} />;
+        } else if (item.description === 'Clear') {
+          weatherIcon = <FontAwesomeIcon icon={faSun} />;
+        } else if (item.description === 'Clouds') {
+          weatherIcon = <FontAwesomeIcon icon={faCloud} />;
+        } else {
+          weatherIcon = <FontAwesomeIcon icon={faSmog} />;
+        }
+    
+        return (
+          <div key={index} className="forecast">
+            <div className="flex-forecast">
+            <p>{moment(item.dt_txt).format("dddd")}</p>
+          
+            <WeatherIcon style={{fontSize:25,marginTop:4}}>{weatherIcon}</WeatherIcon>
+    
+            <p>
+              {item.temperature} &deg;C
+            </p>
+            </div>
+          </div>
+        )
+      })
     return (
         <div>Forecast</div>
     )
