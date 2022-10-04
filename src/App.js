2e102bb8b6e498e54888b5104886d07cdb7a39fe
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-10-04 20:40:57 +0300

    set error message

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   const [longitude, setLongitude] = useState([]);
   const [weatherData, setWeatherData] = useState([]);
   const [forecast, setForecast] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(function (position) {
@@ -16,7 +17,23 @@ function App() {
       setLongitude(position.coords.longitude);
     });
 
+    getWeather(lat, long)
+      .then(weather => {
+        setWeatherData(weather);
+        setError(null);
+      })
+      .catch(err => {
+        setError(err.message);
+      });
 
+    getForecast(lat, long)
+      .then(data => {
+        setForecast(data);
+        setError(null);
+      })
+      .catch(err => {
+        setError(err.message);
+      });
 
 
 
@@ -31,38 +48,38 @@ function App() {
           return mappedData;
         }
       });
-    }
-
-    function getForecast(lat, long) {
-      return fetch(
-        `${process.env.REACT_APP_API_URL}/forecast/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
-      )
-        .then(res => handleResponse(res))
-        .then(forecastData => {
-          if (Object.entries(forecastData).length) {
-            return forecastData.list
-              .filter(forecast => forecast.dt_txt.match(/09:00:00/))
-              .map(mapDataToWeatherInterface);
-          }
-        });
-    }
-
-    return (
-      <div className="App">
-        {(typeof data.main != 'undefined') ? (
-          <div>
-            <Weather weatherData={data} />
-            <Forecast forecast={forecast} weatherData={weatherData} />
-          </div>
-        ) : (
-          <div>
-            <Dimmer active>
-              <Loader>Loading..</Loader>
-            </Dimmer>
-          </div>
-        )}
-      </div>
-    );
   }
 
-  export default App;
+  function getForecast(lat, long) {
+    return fetch(
+      `${process.env.REACT_APP_API_URL}/forecast/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
+    )
+      .then(res => handleResponse(res))
+      .then(forecastData => {
+        if (Object.entries(forecastData).length) {
+          return forecastData.list
+            .filter(forecast => forecast.dt_txt.match(/09:00:00/))
+            .map(mapDataToWeatherInterface);
+        }
+      });
+  }
+
+  return (
+    <div className="App">
+      {(typeof data.main != 'undefined') ? (
+        <div>
+          <Weather weatherData={data} />
+          <Forecast forecast={forecast} weatherData={weatherData} />
+        </div>
+      ) : (
+        <div>
+          <Dimmer active>
+            <Loader>Loading..</Loader>
+          </Dimmer>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
