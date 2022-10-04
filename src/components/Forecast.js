dc184b43b5c900a29dee2c8719bff31a4493b09f
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-10-04 20:57:07 +0300

    import in forecast

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import {
+    List
+} from "@material-ui/core";
+import moment from 'moment';
+import './styles.css';
+import {
+    faCloud,
+    faBolt,
+    faCloudRain,
+    faCloudShowersHeavy,
+    faSnowflake,
+    faSun,
+    faSmog,
+} from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Forecast = () => {
-  return (
-    <div>Forecast</div>
-  )
+    return (
+        <div>Forecast</div>
+    )
 }
 
 export default Forecast
