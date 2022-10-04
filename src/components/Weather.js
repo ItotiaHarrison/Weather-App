dff76bacdfcad3d884963297878cb6a42f2d6a50
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-10-04 16:33:48 +0300

    if statements

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -10,9 +10,29 @@ import {
     faSun,
     faSmog,
 } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 function Weather({ weatherData }) {
+
+    let weatherData = null;
+
+    if (weatherData.weather[0].main === 'Snow') {
+        weatherIcon = <FontAwesomeIcon icon={faSnowflake} />;
+    } else if (weatherData.weather[0].main === 'Clouds') {
+        weatherIcon = <FontAwesomeIcon icon={faCloud} />;
+    } else if (weatherData.weather[0].main === 'Drizzle') {
+        weatherIcon = <FontAwesomeIcon icon={faCloudRain} />;
+    } else if (weatherData.weather[0].main === 'Rain') {
+        weatherIcon = <FontAwesomeIcon icon={faCloudShowersHeavy} />;
+    } else if (weatherData.weather[0].main === 'Thunderstorm') {
+        weatherIcon = <FontAwesomeIcon icon={faBolt} />;
+    } else if (weatherData.weather[0].main === 'Clear') {
+        weatherIcon = <FontAwesomeIcon icon={faSun} />;
+    } else {
+        weatherIcon = <FontAwesomeIcon icon={faSmog} />;
+    }
+
     return (
         <div>Weather</div>
     )
